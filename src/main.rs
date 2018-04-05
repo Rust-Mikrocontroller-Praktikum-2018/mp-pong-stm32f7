95d2514334eabcb3e5db80c5b8e50707384e0f60
Niko16 <na_mach@online.de>
2018-04-05 10:24:10 +0200

    minimal start code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(compiler_builtins_lib)]
+
+extern crate stm32f7_discovery as stm32f7;
+extern crate compiler_builtins;
+extern crate r0;
+
+use stm32f7::{system_clock, board, embedded};
+
+#[no_mangle]
+pub unsafe extern "C" fn reset() -> ! {
+    extern "C" {
+        static __DATA_LOAD: u32;
+        static __DATA_END: u32;
+        static mut __DATA_START: u32;
+
+        static mut __BSS_START: u32;
+        static mut __BSS_END: u32;
+    }
+
+    let data_load = &__DATA_LOAD;
+    let data_start = &mut __DATA_START;
+    let data_end = &__DATA_END;
+
+    let bss_start = &mut __BSS_START;
+    let bss_end = &__BSS_END;
+
+    // initializes the .data section
+    // (copy the data segment initializers from flash to RAM)
+    r0::init_data(data_start, data_end, data_load);
+    // zeroes the .bss section
+    r0::zero_bss(bss_start, bss_end);
+
+    // Initialize the floating point unit
+    let scb = stm32f7::cortex_m::peripheral::scb_mut();
+    scb.cpacr.modify(|v| v | 0b1111 << 20);
+
+    main(board::hw());
+}
+
+fn main(hw: board::Hardware) -> ! {
+
+    let board::Hardware {
+        rcc,
+        pwr,
+        flash,
+        ..
+    } = hw;
+
+    
+
+    system_clock::init(rcc, pwr, flash);
+
+    
+    loop {
+        
+    }
 }
