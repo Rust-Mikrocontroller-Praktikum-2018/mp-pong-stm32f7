46b342f99b80c603f9feb966425d9aa7b7caa72d
bitowl <git@bitowl.de>
2018-04-08 01:02:39 +0200

    Use release build
    This reverts commit dec754c320988bbdb2056b9ae3e2fbedc2c6ccce.

--- gdb.sh
+++ gdb.sh
@@ -5,4 +5,4 @@ set -e
 echo "Please run 'st-util' in another terminal window (you might need sudo)"
 echo ""
 
-arm-none-eabi-gdb -iex 'add-auto-load-safe-path .' -ex "tar ext :4242" -ex "load-reset" target/stm32f7/debug/mp-pong-stm32f7
+arm-none-eabi-gdb -iex 'add-auto-load-safe-path .' -ex "tar ext :4242" -ex "load-reset" target/stm32f7/release/mp-pong-stm32f7
