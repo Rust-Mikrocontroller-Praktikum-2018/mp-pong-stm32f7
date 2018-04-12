6b949c22ad09256f2046c452e74113a17a492bff
Niko16 <na_mach@online.de>
2018-04-12 17:27:39 +0200

    physics improved

--- src/physics.rs
+++ src/physics.rs
@@ -1,4 +1,6 @@
 use ball::BALL_RADIUS;
+use lcd::HEIGHT;
+use lcd::WIDTH;
 use network;
 use racket::RACKET_HEIGHT;
 use racket::RACKET_WIDTH;
@@ -12,17 +14,21 @@ pub fn calculate_physics(
     let racket_width = RACKET_WIDTH as i16;
     let racket_height = RACKET_HEIGHT as i16;
     let ball_radius = BALL_RADIUS as i16;
+    let height = HEIGHT as i16;
+    let width = WIDTH as i16;
     let ball: &mut network::BallPacket = &mut server_gamestate.ball;
     let x_pos_new = ball.x + ball.x_vel;
     let y_pos_new = ball.y + ball.y_vel;
 
     let mut touches_racket_face: bool = false;
-let mut touches_racket_side: bool = false;
+    let mut touches_racket_upper_side: bool = false;
+    let mut touches_racket_under_side: bool = false;
+    let mut in_goal: bool = false;
     // Racket Positions
     // for each player check whether to move up, down or not at all
     for i in 0..2 {
         let mut racket_pos = server_gamestate.rackets[i].y;
-
+        let racket_pos_old = racket_pos;
         let mut input_direction = inputs[i].goal_y - racket_pos;
 
         if input_direction != 0 {
@@ -37,8 +43,8 @@ let mut touches_racket_side: bool = false;
             // keep racket in bounds
             if racket_pos < racket_height {
                 racket_pos = racket_height;
-            } else if racket_pos > 271 - racket_height {
-                racket_pos = 271 - racket_height;
+            } else if racket_pos > height - 1 - racket_height {
+                racket_pos = height - 1 - racket_height;
             }
             server_gamestate.rackets[i].y = racket_pos;
         }
@@ -50,42 +56,68 @@ let mut touches_racket_side: bool = false;
             racket_width,
             racket_height,
         );
-        //ball touches face of left racket
 
         if overlap_test(rect_ball, rect_racket) {
-            if ball.y+ball_radius<server_gamestate.rackets[i].y-racket_height||ball.y-ball_radius>server_gamestate.rackets[i].y+racket_height{touches_racket_side=true;}
-            else{touches_racket_face = true;}
-
+            //ball touches side of racket
+            if ball.y + ball_radius < racket_pos_old - racket_height {
+                touches_racket_under_side = true;
+            } else if ball.y - ball_radius > racket_pos_old + racket_height {
+                touches_racket_upper_side = true;
+            } else {
+                //ball touches face of racket
+                touches_racket_face = true;
+            }
         }
     }
     //move Ball
-    //if ball touches top or bottom wall orr racket side
-    if y_pos_new < ball_radius || y_pos_new > 271 - ball_radius||touches_racket_side {
+    //if ball touches top or bottom wall
+    if y_pos_new < ball_radius || y_pos_new > height - 1 - ball_radius {
         ball.y_vel *= -1;
-    } else {
-        //new position=old position+velocity
-        ball.y += ball.y_vel;
     }
+    //if ball touches racket side
+    if touches_racket_upper_side {
+        ball.y_vel = -abs(ball.y_vel);
+    } else if touches_racket_under_side {
+        ball.y_vel = abs(ball.y_vel);
+    }
+    //new position=old position+velocity
+    ball.y += ball.y_vel;
+
     //check for goals
-    if x_pos_new <= ball_radius || x_pos_new >= 479 - ball_radius {
-        // if ball touches goal increase score and reset ball position
-        if x_pos_new <= ball_radius {
-            server_gamestate.score[1] += 1;
-        }
-        if x_pos_new >= 479 - ball_radius {
-            server_gamestate.score[0] += 1;
-        }
-        ball.reset();
-    }// if ball touches racket face invert x_vel
+    if x_pos_new <= ball_radius || x_pos_new >= width - 1 - ball_radius {
+        in_goal = true;
+    }
+    // if ball touches racket face invert x_vel
     else if touches_racket_face {
-        ball.x_vel *= -1;
+        if x_pos_new > width as i16 / 2 {
+            ball.x_vel = -abs(ball.x_vel);
+        } else {
+            ball.x_vel = abs(ball.x_vel);
+        }
     }
     //new position=old position+velocity
     ball.x += ball.x_vel;
+    // if ball touches goal increase score and reset ball position
+    if in_goal {
+        if x_pos_new <= ball_radius {
+            server_gamestate.score[1] += 1;
+        }
+        if x_pos_new >= width - 1 - ball_radius {
+            server_gamestate.score[0] += 1;
+        }
+        ball.reset();
+    }
 }
 fn overlap_test(rectangle1: Rectangle, rectangle2: Rectangle) -> bool {
     !(rectangle2.right < rectangle1.left || rectangle2.left > rectangle1.right
         || rectangle2.top > rectangle1.bottom || rectangle2.bottom < rectangle1.top)
+}
+fn abs(value: i16) -> i16 {
+    if value < 0 {
+        -value
+    } else {
+        value
+    }
 }
 
 struct Rectangle {
